b307e959775534e7a0f59dbac95e141227b26afc
lijianwei <lijianwei@xkw.cn>
2019-10-16 11:07:43 +0800

    加速

--- src/main/java/org/mzj/test/MyApplication.java
+++ src/main/java/org/mzj/test/MyApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
- *  加入 -XX:TieredStopAtLevel=1 -noverify 加快启动速度
+ *  启动加速的VM参数:
+ *  -XX:TieredStopAtLevel=1  多层编译编译到第一层,只用C1
+ *  -noverify  关闭字节码校验
  * @author mzj
  */
 @SpringBootApplication
@@ -23,12 +22,12 @@ public class MyApplication {
 		return factory -> factory.setPort(8081);
 	}
 	
-	@Configuration
-    @Profile("local")
-    @ComponentScan(lazyInit = true)
-    static class LocalConfig {
-		static {
-			System.out.println("延迟加载，加速应用启动。");
-		}
-    }
+//	@Configuration
+//    @Profile("local")
+//    @ComponentScan(lazyInit = true)
+//    static class LocalConfig {
+//		static {
+//			System.out.println("延迟加载，加速应用启动。");
+//		}
+//    }
 }
